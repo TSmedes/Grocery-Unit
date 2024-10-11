4431922a95bdac3ed066c209fe8aecfc54727255
TSmedes <144851348+TSmedes@users.noreply.github.com>
2024-10-10 19:26:11 -0700

    keys

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  console.log(process.env.VITE_SUPABASE_URL),
   import.meta.env.VITE_SUPABASE_URL || "",
   import.meta.env.VITE_SUPABASE_ANON_KEY || "",
   {
