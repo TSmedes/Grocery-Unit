79ef121459efb53213011d5ff2829d63ec128582
TSmedes <144851348+TSmedes@users.noreply.github.com>
2024-10-10 17:35:00 -0700

    Build config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-module.exports = {
-  root: 'src',
+  // root: 'src',
   build: {
-    outDir: '../build',
+    outDir: './build',
   }
-}
+})
